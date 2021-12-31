eda2a70006b88fffc55c617b4f13a71a1e79253a
Phil Curran <pecurran@hotmail.com>
2021-12-31 12:23:23 -0800

    updates and fixes for addition of bulma & scss / sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -23,12 +24,23 @@ module.exports = {
       template: "./src/index.html",
       inject: "body",
     }),
+    new MiniCssExtractPlugin({
+      // filename: "css/mystyles.css",
+      filename: "./src/css/styles.css",
+    }),
   ],
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
       {
         test: /\.js$/,
