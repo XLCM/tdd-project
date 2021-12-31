9ee74a56a4ee84b0b6ca8ef04d6c68b775fdffaf
Phil Curran <pecurran@hotmail.com>
2021-12-31 10:39:02 -0800

    updated config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/js/scripts.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "TDD Project",
       template: "./src/index.html",
