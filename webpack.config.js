30745311f1f46560c79aab03ded9a0d5c2c23ffb
Phil Curran <pecurran@hotmail.com>
2021-12-31 11:04:06 -0800

    updates for eslint addition

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,11 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+      },
     ],
   },
 };
