f34f3ec4b48a22872d99c971c4be052810da296c
Phil Curran <pecurran@hotmail.com>
2022-01-01 13:38:52 -0800

    failing test for getting a now date

--- __tests__/planetaryCalculator.test.js
+++ __tests__/planetaryCalculator.test.js
@@ -1,6 +1,12 @@
 import { PlanetaryCalculator } from "../src/js/planetaryCalculator.js";
 
 describe("PlanetaryCalculator", () => {
+  test("should create a 'now' date in ms to measure from", () => {
+    let now = new Date().getTime();
+    let planetaryCalculator = new PlanetaryCalculator(now);
+    expect(planetaryCalculator.now).toEqual(now);
+  });
+
   test("should create new object with birthday given a date and return in ms", () => {
     let birthday = "1978 September 4";
     let birthDate = new PlanetaryCalculator(birthday);
